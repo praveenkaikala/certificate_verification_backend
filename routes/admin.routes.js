e4343534b2c9d890836b6de2dfb9c44d5f8013da
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2026-01-24 10:41:19 +0530

    admin routes

---
+++ routes/admin.routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const adminInstituteController = require("../controllers/admin.controller");
+const { adminAuth } = require("../middleware/auth.middleware");
+
+router.get("/institutes", adminAuth, adminInstituteController.getInstitutes);
+router.get("/institutes/:instituteId", adminAuth, adminInstituteController.getInstituteById);
+router.put("/institutes/:instituteId/verify", adminAuth, adminInstituteController.verifyInstitute);
+router.delete("/institutes/:instituteId", adminAuth, adminInstituteController.deleteInstitute);
+
+module.exports = router;
